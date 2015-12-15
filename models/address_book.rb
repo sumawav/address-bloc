9e646776bd7a6209a113f8d42cc5fca854b4c335
Souma Mondal <mondal.souma@gmail.com>
2015-12-15 04:37:47 -0600

    Checkpoint 22 Searching: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -26,7 +26,27 @@ class AddressBook
         csv.each do |row|
             row_hash = row.to_hash
             add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-        end    
+        end
+    end
+
+    def binary_search(name)
+        lower = 0
+        upper = @entries.length - 1
+
+        while lower <= upper
+            mid = (lower + upper) / 2
+            mid_name = @entries[mid].name
+
+            if name == mid_name
+                return @entries[mid]
+            elsif name < mid_name
+                upper = mid -1
+            elsif name > mid_name
+                lower = mid + 1
+            end
+        end
+
+        return nil
     end
 
 end
